e0ce1ff5a9dd49dced595d9a32799ccb9a73df4a
Luis Gustavo Santos <spartatecnologia@gmail.com>
2018-05-06 19:02:35 -0300

    encaminhando

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+const mysql = require('mysql2/promise')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const account = require('./account')
+const admin = require('./admin')
+
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({
+    secret: 'spartadev',
+    resave: true,
+    saveUninitialized:true
+}))
+app.set('view engine', 'ejs')
+
+
+
+const init = async() => {
+    const connection = await mysql.createConnection({
+        host: '127.0.0.1',
+        user: 'root',
+        password:'',
+        database:'futibaclub'
+    })
+
+    app.use((req, res, next) => {
+        if(req.session.user){
+            res.locals.user = req.session.user
+        }else{
+            res.locals.user = false
+        }
+        next()
+    })
+
+    app.use(account(connection))
+    app.use('/admin', admin(connection))
+
+    app.listen(3001, err=> {
+        console.log('Futiba Club server is running...')
+    })
+}
+init()
+
